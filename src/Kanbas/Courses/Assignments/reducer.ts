c9d17494160aebc5bb8cbc6a6b4ad47d1642fc92
Keying <Keying01152000@gmail.com>
2024-11-23 15:16:36 -0800

    Initial commit for new repository

---
+++ src/Kanbas/Courses/Assignments/reducer.ts
@@ -0,0 +1,48 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { assignments } from "../../Database";
+const initialState = {
+  assignments: [],
+};
+const assignmentsSlice = createSlice({
+  name: "assignemnts",
+  initialState,
+  // reducers 包含了3个  reducer 函数，分别用于添加、更新和编辑作业。
+  reducers: {
+    setAssignments: (state, action) => {
+      state.assignments = action.payload;
+    },
+    addAssignment: (state, { payload: assignment }) => {
+      const newAssignment: any = {
+        // // 使用时间戳作为唯一ID
+        _id: new Date().getTime().toString(),
+        title: assignment.title,
+        course: assignment.course,
+        description:assignment.description,
+        point: assignment.point,
+        dueDate: assignment.dueDate,
+        availableFromDate: assignment.availableFromDate,
+        availableUntilDate: assignment.availableUntilDate
+      };
+      state.assignments = [...state.assignments, newAssignment] as any;
+    },
+
+    updateAssignment: (state, { payload: assignment }) => {
+      state.assignments = state.assignments.map((a: any) =>
+        a._id === assignment._id ? { ...a, ...assignment } : a
+      ) as any;
+    },
+
+    editAssignment: (state, { payload: assignmentId }) => {
+      state.assignments = state.assignments.map((a: any) =>
+        a._id === assignmentId ? { ...a, editing: true } : a
+      ) as any;
+    },
+    deleteAssignment: (state, { payload: assignmentId }) => {
+      state.assignments = state.assignments.filter(
+        (m: any) => m._id !== assignmentId);
+    },
+  },
+});
+export const { setAssignments, addAssignment, updateAssignment, editAssignment ,deleteAssignment} =
+  assignmentsSlice.actions;
+export default assignmentsSlice.reducer;
---
+++ src/Kanbas/index.tsx
@@ -0,0 +1,126 @@
+import { Routes, Route, Navigate } from "react-router";
+import Account from "./Account";
+import Dashboard from "./Dashboard";
+import KanbasNavigation from "./Navigation";
+import Courses from "./Courses";
+import PeopleTable from "./Courses/People/Table";
+import "./styles.css";
+// import * as db from "./Database";
+import { useEffect, useState } from "react";
+import store from "./store";
+import { Provider, useSelector } from "react-redux";
+import ProtectedRoute from "./Account/ProtectedRoute";
+import Session from "./Account/Session";
+import * as client from "./Courses/client";
+import * as userClient from "./Account/client";
+import * as courseClient from "./Courses/client";
+
+export default function Kanbas() {
+  const [courses, setCourses] = useState<any[]>([]);
+  const [course, setCourse] = useState<any>({
+    _id: "1234",
+    name: "",
+    number: "New Number",
+    startDate: "2023-09-10",
+    endDate: "2023-12-15",
+    department: "D123",
+    credits: 4,
+    description: "",
+    image:"/images/reactjs.jpg",
+  });
+  // const addCourse = async () => {...};
+
+  const addNewCourse = async() => {
+    // const newCourse: any ={
+    // _id: new Date().getTime().toString(),
+    // name: course.name,
+    // number: course.number,
+    // description: course.description,
+    // image:"/images/reactjs.jpg",
+    // }
+    const newCourse = await userClient.createCourse(course);
+    setCourses((courses) => [
+      ...courses,
+      newCourse 
+    ]);
+  };
+
+
+  const deleteCourse = async (courseId: any) => {
+    const status = await courseClient.deleteCourse(courseId);
+    setCourses(courses.filter((course) => course._id !== courseId));
+  };
+
+  const updateCourse = async() => {
+    await courseClient.updateCourse(course);
+    setCourses(
+      courses.map((c) => {
+        if (c._id === course._id) {
+          return course;
+        } else {
+          return c;
+        }
+      })
+    );
+  };
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+
+  const fetchCourses = async () => {
+    try {
+      const courses = await courseClient.fetchAllCourses();
+      setCourses(courses);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCourses();
+  }, [currentUser]);
+
+
+  return (
+    <Session>
+    <Provider store={store}>
+      <div id="wd-kanbas">
+        {/* <h1>Kanbas</h1> */}
+        <KanbasNavigation />
+        {/* p-3 means padding's value is 3 */}
+        <div className="wd-main-content-offset p-3">
+          <Routes>
+            <Route path="/" element={<Navigate to="Account" />} />
+            <Route path="/Account/*" element={<Account />} />
+            {/* <ProtectedRoute> 用于对页面或组件进行访问控制。如果条件不满足，则会阻止用户访问，并将用户重定向到登录页面。 */}
+            <Route
+              path="/Dashboard"
+              element={
+                <ProtectedRoute>
+                  <Dashboard
+                    courses={courses}
+                    course={course}
+                    setCourse={setCourse}
+                    addNewCourse={addNewCourse}
+                    deleteCourse={deleteCourse}
+                    updateCourse={updateCourse}
+                  />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/Courses/:cid/*"
+              element={
+                <ProtectedRoute>
+                  <Courses courses={courses} />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/Calendar" element={<h1>Calendar</h1>} />
+            <Route path="/Inbox" element={<h1>Inbox</h1>} />
+            {/* <Route path="/Courses/:cid/People" element={<PeopleTable />} /> */}
+          </Routes>
+        </div>
+      </div>
+    </Provider>
+    </Session>
+  );
+}
